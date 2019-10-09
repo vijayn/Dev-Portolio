4ab663024b44304d27b4078b0414d3321a272109
Vijay N <vijay@Vijays-MacBook-Pro.local>
2019-10-09 17:14:13 +0530

    Implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio was updated'}
+        format.html { redirect_to portfolios_url, notice: 'Your portfolio was updated'}
       else
         format.html {render :edit}
       end
@@ -37,6 +37,14 @@ class PortfoliosController < ApplicationController
 
   def show
     @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html {redirect_to portfolios_url, notice: 'Portfolio is deleted succesfully'}
+    end
   end
 
 end
